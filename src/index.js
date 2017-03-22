20690eb0ef4bb76c960225148c451ac699da6262
Nick Ooms <oomsni@gmail.com>
2017-03-22 02:36:13 +0100

    added react

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import store from './store';
+import App from './containers/App';
+import Home from './containers/Home';
+import Street from './components/Street';
+import HouseNumberList from './components/HouseNumberList';
+
+import './static/normalize.css';
+import './static/index.css';
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+/* eslint-disable no-undef */
+const root = document.getElementById('root');
+/* eslint-enable */
+
+render((
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        {/* Your routes here */}
+        <Route path="street/:streetId" component={Street} />
+        <Route path="housenumbers/:streetId" component={HouseNumberList} />
+      </Route>
+    </Router>
+  </Provider>
+), root);
